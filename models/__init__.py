e139bfb89b27e60a2c1e4b971b378e5ec6e3ce78
Moses Kisya <118076277+kishea-dev@users.noreply.github.com>
2023-06-22 13:50:25 -0700

    storage fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,13 @@
-from models.engine.file_storage import FileStorage
+from os import getenv
 
-storage = FileStorage()
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+if storage_type == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()
